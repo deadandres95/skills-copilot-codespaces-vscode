33b33b0e8b05dbe7fa7318270736449638baa53f
Andrés Jiménez <ajimenezf95@gmail.com>
2023-11-09 02:51:00 +0000

    Copilot Third commit

--- comments.js
+++ comments.js
@@ -1,3 +1,23 @@
-// create web server with node.js
-// run: node comments.js
-// output: http://
+// create web server
+
+// import modules
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// set view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// set static files
+app.use(express.static('public'));
+
+// set routes
+app.use('/', require('./routes/index'));
+app.use('/comments', require('./routes/comments'));
+
+// listen to port
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
